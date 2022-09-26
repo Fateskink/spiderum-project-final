4feeda5030f06593427e91a3ddb91640c7693056
fateskink <shaynenguyen9@gmail.com>
2022-09-26 11:50:46 +0700

    add index to posts created_at

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_26_040016) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_26_044733) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_26_040016) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "view"
+    t.index ["created_at"], name: "index_posts_on_created_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
