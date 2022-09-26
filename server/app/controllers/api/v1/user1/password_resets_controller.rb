acdd0771584e72a5da13c93f16d4802dc4effcee
fateskink <shaynenguyen9@gmail.com>
2022-09-26 10:53:37 +0700

    add reset password ctrl

---
+++ server/app/controllers/api/v1/user1/password_resets_controller.rb
@@ -0,0 +1,13 @@
+module Api
+  module V1
+    module User1
+      class PasswordResetsController < ApplicationController
+        def new
+        end
+
+        def edit
+        end
+      end
+    end
+  end
+end
