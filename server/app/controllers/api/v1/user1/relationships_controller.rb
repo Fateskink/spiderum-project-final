1bf313b4392a0923f04becabafbb00502ec2f644
fateskink <shaynenguyen9@gmail.com>
2022-09-27 09:06:59 +0700

    show followers, followings

--- server/app/controllers/api/v1/user1/relationships_controller.rb
+++ server/app/controllers/api/v1/user1/relationships_controller.rb
@@ -2,16 +2,17 @@ module Api
   module V1
     module User1
       class RelationshipsController < ApplicationController
-
+        
         def create
           current_user.active_relationships.build(followed_id: @user.id)
+          render json: { message: 'Follow' }, status: :ok
         end
 
         def destroy
-        @relationship = current_user.active_relationships.find_by(followed_id: @user.id)
-          if @relationship.valid? 
+          @relationship = current_user.active_relationships.find_by(followed_id: @user.id)
+          if @relationship.valid?
             @relationship.destroy
-            render json: {message: "Unfollow"}, status: :ok
+            render json: { message: 'Unfollow' }, status: :ok
           end
         end
       end
