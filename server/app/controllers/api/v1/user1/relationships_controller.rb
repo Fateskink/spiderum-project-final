2c7ed5e8b2dabcdd4c8f0c0d488de4e9e72b7829
fateskink <shaynenguyen9@gmail.com>
2022-09-27 09:00:10 +0700

    follow & unfollow

---
+++ server/app/controllers/api/v1/user1/relationships_controller.rb
@@ -0,0 +1,20 @@
+module Api
+  module V1
+    module User1
+      class RelationshipsController < ApplicationController
+
+        def create
+          current_user.active_relationships.build(followed_id: @user.id)
+        end
+
+        def destroy
+        @relationship = current_user.active_relationships.find_by(followed_id: @user.id)
+          if @relationship.valid? 
+            @relationship.destroy
+            render json: {message: "Unfollow"}, status: :ok
+          end
+        end
+      end
+    end
+  end
+end
