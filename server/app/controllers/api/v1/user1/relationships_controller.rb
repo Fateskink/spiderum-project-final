9396a8dd9ce82f9a30481371618a90d605d6fcbe
fateskink <shaynenguyen9@gmail.com>
2022-09-29 10:38:42 +0700

    follow done

--- server/app/controllers/api/v1/user1/relationships_controller.rb
+++ server/app/controllers/api/v1/user1/relationships_controller.rb
@@ -4,41 +4,38 @@ module Api
       class RelationshipsController < ApplicationController
         before_action :logged_in_user
 
-        def create
-          @user = User.find(params[:followed_id])
-          current_user.follow(@user)
-
-          # current_user.active_relationships.build(follower_id: @current_user.id,followed_id: @user.id)
-          render json: { message: 'Follow' }, status: :ok
-        end
-
         # def create
         #   @user = User.find(params[:followed_id])
         #   current_user.follow(@user)
-        #   respond_to do |format|
-        #     format.html { redirect_to @user }
-        #     format.js
-        #   end
+        #   # current_user.active_relationships.build(follower_id: @current_user.id,followed_id: @user.id)
+        #   render json: { message: 'Follow' }, status: :ok
         # end
-      
+
+        def create
+          @user = User.find(params[:user_id])
+          unless @user.followers.find_by(id: params[:id])
+            User.find(params[:id]).follow(@user)
+            render json: { message: 'Follow' }, status: :ok
+          end
+        end
+
         # def destroy
-        #   @user = Relationship.find(params[:id]).followed
-        #   
-        #   respond_to do |format|
-        #     format.html { redirect_to @user }
-        #     format.js
-        #   end
+        #   @user = Relationship.find_by_id(id: params[:followed_id])
+        #   current_user.unfollow(@user)
+        #   render json: { message: 'Unfollow' }, status: :ok
+        #   # @relationship = current_user.active_relationships.find_by(followed_id: @user.id)
+        #   # if @relationship.valid?
+        #   #   @relationship.destroy
+        #   #   render json: { message: 'Unfollow' }, status: :ok
+        #   # end
         # end
 
         def destroy
-          @user = Relationship.find(params[:id]).followed
-          current_user.unfollow(@user)
-          render json: { message: 'Unfollow' }, status: :ok
-          # @relationship = current_user.active_relationships.find_by(followed_id: @user.id)
-          # if @relationship.valid?
-          #   @relationship.destroy
-          #   render json: { message: 'Unfollow' }, status: :ok
-          # end
+          @user = User.find(params[:user_id])
+          if @user.followers.find_by(id: params[:id])
+            User.find(params[:id]).unfollow(@user)
+            render json: { message: 'Unfollow' }, status: :ok
+          end
         end
 
       end
