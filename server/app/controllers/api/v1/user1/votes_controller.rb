6d5dab0088b23ad7219fd73ac2497dffa7bc7726
fateskink <shaynenguyen9@gmail.com>
2022-10-03 12:06:05 +0700

    lunch

--- server/app/controllers/api/v1/user1/votes_controller.rb
+++ server/app/controllers/api/v1/user1/votes_controller.rb
@@ -7,36 +7,30 @@ module Api
 
         def create
           @vote = @votetable.votes.build
-          # @vote = Vote.new
-          @vote.user_id = 2
-          # render json: @vote
-          # @vote.update(upvote: @vote.upvote = 1)
-          render json: { vote: @vote }, status: :ok if @vote.save
+          @vote.user_id = current_user.id
+          @vote.update(vote_score: @vote.vote_score + 1)
+          if @vote.save
+            render json: { vote: @vote }, status: :ok
+          else
+            render json: @vote.errors.full_messages, status: :unprocessable_entity
+          end
         end
 
-      #   def vote_up
-      #     @list = List.find(params[:list_id])
-      #     @vote = Vote.find_or_create_by(list_id: params[:id], user_id: current_user.id)
-      #     Vote.increment_counter(:vote_count, @vote)
-      #     redirect_to list_path(@list), notice: 'Voted Up.'
-      # end
-  
-      # def vote_down
-      #     @list = List.find(params[:list_id])
-      #     @vote = Vote.find_or_create_by(list_id: params[:id], user_id: current_user.id)
-      #     Vote.decrement_counter(:vote_count, @vote)
-      #     redirect_to list_path(@list), notice: 'Voted Down.'    
-      # end
-
-        def clear_vote(some_vote)
-          Vote.destroy(some_vote)
+        def destroy
+          @vote = Vote.find_by(params[:vote_id])
+          if @vote.vote_score == 0
+            @vote.update(vote_score: @vote.vote_score - 1)
+          elsif @vote.vote_score == 1
+            @vote.update(vote_score: @vote.vote_score - 2)
+          end
+            if @vote.save
+              render json: { vote: @vote }, status: :ok
+            else
+              render json: @vote.errors.full_messages, status: :unprocessable_entity
+            end
         end
 
-      private
-
-        def vote_params
-          params.require(:vote).permit(:vote_score)
-        end
+        private
 
         def find_votetable
           if params[:comment_id]
