9f779f585f9eedf29923ef2cfa57c1201e819120
fateskink <shaynenguyen9@gmail.com>
2022-10-03 09:02:55 +0700

    add ranking ctrl scafford

--- server/app/controllers/api/v1/user1/rankings_controller.rb
+++ server/app/controllers/api/v1/user1/rankings_controller.rb
@@ -2,6 +2,12 @@ module Api
   module V1
     module User1
       class RankingsController < ApplicationController
+        # post.readers.where('created_at >= ?', 5.minutes.ago)
+        def create
+        end
+
+        def destroy
+        end
         
       end
     end
