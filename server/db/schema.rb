534c75488e9ab3743058b45bc800d141e58b0916
fateskink <shaynenguyen9@gmail.com>
2022-10-04 09:44:41 +0700

    delete remmeber_digest from user

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_03_153937) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_04_024242) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -111,7 +111,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_03_153937) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "activation_digest"
     t.boolean "activated", default: false
