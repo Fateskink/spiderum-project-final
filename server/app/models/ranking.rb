2be530e82c99ef480fa56246da73adfd0ae1d5c7
fateskink <shaynenguyen9@gmail.com>
2022-10-06 16:01:52 +0700

    ranking without time out action

--- server/app/models/ranking.rb
+++ server/app/models/ranking.rb
@@ -2,15 +2,17 @@ class Ranking < ApplicationRecord
   belongs_to :favourite
   belongs_to :post
 
+  # @post.readers.where('reader_links.created_at >= ?', 45.minutes.ago).any?
+  # ranking.posts.where('created_at >=?', 1.month.ago)
+  # post.readers.where('created_at >= ?', 5.minutes.ago)
   # clean ranking when time out (1 month)
   # def clean
   #   time_range = (Time.now.midnight - 1.month)..Time.now.midnight
-  #   @posts = Post.joins(:favourite)
-  #                .select('favourites.*, COUNT(posts.id) as ranking')
-  #                .group('favourites.id')
+  #   @posts = Post.joins(:rankings)
+  #                .select('rankings.*, COUNT(posts.id) as ranking')
+  #                .group('rankings.id')
   #                .where('posts.created_at' => time_range)
   #                .order('ranking DESC')
   #                .limit(10)
-  #   render json: @posts, status: :ok
   # end
 end
