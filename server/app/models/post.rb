8b702ff118cdf38e7bc7de370421ac4dcb388d5e
fateskink <shaynenguyen9@gmail.com>
2022-10-06 12:02:23 +0700

    add notification to post for followers

--- server/app/models/post.rb
+++ server/app/models/post.rb
@@ -16,10 +16,23 @@ class Post < ApplicationRecord
   validates :content, presence: true
   validates :image, content_type: { in: %w[image/jpeg image/gif image/png], message: "must be a valid image format" },
                             size: { less_than: 5.megabytes, message: "should be less than 5MB" }
+  after_create :create_notifications
 
   # Returns a resized image for display.
   def display_image
     image.variant(resize_to_limit: [1000, 1000]) # maybe change image size, dependent on future feature
+  end
+
+  # All users is following the post owner
+  def recipients
+    @recipients = self.user.followers
+  end
+
+  def create_notifications
+    recipients.each do |recipient|
+      Notification.create(recipient: recipient, actor: self.user,
+        action: 'posted', notificationable: self)
+    end
   end
 
 end
