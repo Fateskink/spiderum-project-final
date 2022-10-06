91bd64f17ad82a326d359a15ed84eab841119221
fateskink <shaynenguyen9@gmail.com>
2022-10-06 19:54:59 +0700

    update ranking

--- server/app/models/ranking.rb
+++ server/app/models/ranking.rb
@@ -6,13 +6,5 @@ class Ranking < ApplicationRecord
   # ranking.posts.where('created_at >=?', 1.month.ago)
   # post.readers.where('created_at >= ?', 5.minutes.ago)
   # clean ranking when time out (1 month)
-  # def clean
-  #   time_range = (Time.now.midnight - 1.month)..Time.now.midnight
-  #   @posts = Post.joins(:rankings)
-  #                .select('rankings.*, COUNT(posts.id) as ranking')
-  #                .group('rankings.id')
-  #                .where('posts.created_at' => time_range)
-  #                .order('ranking DESC')
-  #                .limit(10)
-  # end
+
 end
