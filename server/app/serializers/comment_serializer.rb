5d3eea3e3677bef3dae2c00d0f0c32471a7aa918
fateskink <shaynenguyen9@gmail.com>
2022-10-11 10:15:58 +0700

    add vote_sum to comments

--- server/app/serializers/comment_serializer.rb
+++ server/app/serializers/comment_serializer.rb
@@ -2,5 +2,14 @@ class CommentSerializer < ActiveModel::Serializer
   attributes :name, :body, :created_at, :vote_count
 
   belongs_to :post
-  has_many :comments
+  has_many :comment
+
+  def name
+    set_cmt
+    @name = @comment.users.name
+  end
+
+  def set_cmt
+    @comment = Comment.find(params[:id])
+  end
 end
--- server/app/serializers/post_serializer.rb
+++ server/app/serializers/post_serializer.rb
@@ -1,18 +1,13 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :title, :content, :created_at, :view, :name, :favourite_count, :vote_sum
+  attributes :title, :name, :created_at, :content,  :view, :favourite_count, :vote_sum
 
-  belongs_to :user
+  # belongs_to :user
   has_many :comments
   has_many :votes
 
   def name
     set_post
     @name = @post.users.name
-  end
-
-  def vote_sum
-    set_post
-    @vote_sum = @post.votes.sum(:vote_score)
   end
 
   def set_post
