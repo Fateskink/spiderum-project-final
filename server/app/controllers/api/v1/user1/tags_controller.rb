8f8be17d270e9bc40b2dde7ae000b1e91eae6020
fateskink <shaynenguyen9@gmail.com>
2022-10-17 15:52:15 +0700

    add friendly url to tags

--- server/app/controllers/api/v1/user1/tags_controller.rb
+++ server/app/controllers/api/v1/user1/tags_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     module User1
       class TagsController < ApplicationController
+        before_action :authorize
         before_action :admin_user, only: %i[create destroy]
 
         def index
@@ -10,8 +11,7 @@ module Api
         end
 
         def show
-          @tag = Tag.find(params[:id])
-          # @posts = @tag.posts
+          @tag = Tag.friendly.find(params[:id])
           @pagy, @posts = pagy(@tag.posts)
           render json: { metadata: meta_data, posts: @posts }, status: :ok
         end
@@ -35,6 +35,10 @@ module Api
         def tag_params
           params.require(:tag).permit(:tag_name)
         end
+
+        # def admin_user
+        #   @current_user.admin?
+        # end
       end
     end
   end
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_17_044154) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_17_083125) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -69,6 +69,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_044154) do
     t.index ["user_id"], name: "index_favourites_on_user_id"
   end
 
+  create_table "friendly_id_slugs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, length: { slug: 70, scope: 70 }
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", length: { slug: 140 }
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
+
   create_table "messages", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "mes_content"
     t.bigint "user_id", null: false
@@ -123,6 +134,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_044154) do
     t.string "tag_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_tags_on_slug", unique: true
     t.index ["tag_name"], name: "index_tags_on_tag_name", unique: true
   end
 
