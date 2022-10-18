73c65129224f9c23c9c05d67b3af8abbd7d9b503
fateskink <shaynenguyen9@gmail.com>
2022-10-18 10:45:06 +0700

    fix tag serializer

--- server/app/serializers/tag_serializer.rb
+++ server/app/serializers/tag_serializer.rb
@@ -1,4 +1,10 @@
 class TagSerializer < ActiveModel::Serializer
   attributes :tag_name
-  has_many :posts, serializer: PostSerializer
+  has_many :posts
+  class PostSerializer < ActiveModel::Serializer
+    attributes :id, :title, :content, :view, :favourite_count, :vote_sum, :comment_count, :image_url, :created_at  
+    class UserSerializer < ActiveModel::Serializer
+      attributes :name
+    end
+  end
 end
