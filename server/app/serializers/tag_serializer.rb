01a71bfad8c06d906ddf52ffce3666ea25fcd776
fateskink <shaynenguyen9@gmail.com>
2022-10-18 16:39:10 +0700

    done show comment

--- server/app/serializers/tag_serializer.rb
+++ server/app/serializers/tag_serializer.rb
@@ -4,7 +4,7 @@ class TagSerializer < ActiveModel::Serializer
   attributes :tag_name
   has_many :posts
   class PostSerializer < ActiveModel::Serializer
-    attributes :id, :title, :content, :view, :favourite_count, :vote_sum, :comment_count, :image_url, :created_at  
+    attributes :id, :title, :content, :view, :favourite_count, :vote_sum, :comment_count, :image_url, :created_at
     class UserSerializer < ActiveModel::Serializer
       attributes :name
     end
