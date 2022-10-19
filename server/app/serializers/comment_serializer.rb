c707afee6a758465285421143cc8d2968c804f54
fateskink <shaynenguyen9@gmail.com>
2022-10-19 12:01:26 +0700

    update comments serializer

--- server/app/serializers/comment_serializer.rb
+++ server/app/serializers/comment_serializer.rb
@@ -2,4 +2,5 @@ class CommentSerializer < ActiveModel::Serializer
   attributes :id, :body, :created_at, :vote_sum
 
   belongs_to :user, serializer: ::Users::UserLiteSerializer
+  has_many :comments, serializer: self
 end
